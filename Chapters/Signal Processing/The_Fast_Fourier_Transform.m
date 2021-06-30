6a6a5236fd21ccd830ad121b55c2b6e6317c559a
Mark C. Anderson <anderson.mark.az@gmail.com>
2021-06-30 15:19:22 -0600

    Completed FFT part

--- Chapters/Signal Processing/The_Fast_Fourier_Transform.m
+++ Chapters/Signal Processing/The_Fast_Fourier_Transform.m
@@ -67,3 +67,132 @@ h.Position(3:4) = [6.0,3.0];
 savePlots('SavePath',path,...
           'FileTypes',"png",...
           'Titles',"Raw FFT Output")
+      
+%% Normalizing the real part
+
+close all
+
+L = length(waveform);
+Xds = abs(Y./L);
+
+h = figure();
+plot(f,Xds)
+title('Normalized Double-sided Spectrum')
+xlabel('Frequency (Hz)')
+ylabel('Amplitude')
+grid on
+
+h.Position = [2,2,4,3];
+
+ax = gca;
+ax.Position = [0.12, 0.15, 0.80, 0.75];
+
+savePlots('SavePath',path,...
+          'FileTypes',"png")
+      
+%% Making the single-sided version
+
+close all
+
+Xss = Xds(1:L/2) .* 2;
+f = f(1:L/2);
+
+h = figure();
+plot(f,Xss);
+title('Normalized Single-sided Spectrum')
+xlabel('Frequency (Hz)')
+ylabel('Amplitude')
+grid on
+
+h.Position = [2,2,4,3];
+
+ax = gca;
+ax.Position = [0.12, 0.15, 0.80, 0.75];
+
+savePlots('SavePath',path,...
+          'FileTypes',"png")
+      
+%% Using a higher sampling frequency
+
+fs = 1000;
+dt = 1/fs;
+t = 0:dt:2;
+
+waveform = 3.0.*sin(2*pi*f1*t) + 5.0.*sin(2*pi*f2*t) + randn(1,length(t));
+
+Y = fft(waveform);
+f = fs .* linspace(0,1,length(waveform));
+
+L = length(waveform);
+Xds = abs(Y./L);
+
+Xss = Xds(1:L/2) .* 2;
+f = f(1:L/2);
+
+h = figure();
+plot(f,Xss);
+title('Using a Higher Sampling Frequency')
+xlabel('Frequency (Hz)')
+ylabel('Amplitude')
+grid on
+
+h.Position = [2,2,4,3];
+
+ax = gca;
+ax.Position = [0.12, 0.15, 0.80, 0.75];
+ax.XLim = [0,50];
+
+savePlots('SavePath',path,...
+          'FileTypes',"png")
+      
+%% Using different recording lengths
+
+fs = 100;
+dt = 1/fs;
+
+lengths = [0.5,2.0,100];
+
+h = figure();
+h.Position = [2,2,6,3];
+tiles = tiledlayout(1,3);
+
+for i = 1:3
+    
+    nexttile
+    
+    t = 0:dt:lengths(i);
+
+    waveform = 3.0.*sin(2*pi*f1*t) + 5.0.*sin(2*pi*f2*t) + randn(1,length(t));
+
+    Y = fft(waveform);
+    f = fs .* linspace(0,1,length(waveform));
+
+    L = length(waveform);
+    Xds = abs(Y./L);
+
+    Xss = Xds(1:L/2) .* 2;
+    f = f(1:L/2);
+
+    plot(f,Xss);
+    title(strcat(num2str(lengths(i))," s"))
+    xlabel('Frequency (Hz)')
+    ylim([0,5.5])
+    
+    if i == 1
+        ylabel('Amplitude')
+    end
+    grid on
+
+%     ax = gca;
+%     ax.Position = [0.12, 0.15, 0.80, 0.75];
+%     ax.XLim = [0,50];
+    
+end
+
+tiles.Title.String = "Adjusting the Recording Length";
+tiles.Title.FontWeight = 'bold';
+tiles.Title.FontName = 'arial';
+
+savePlots('SavePath',path,...
+          'FileTypes',"png",...
+          'Titles',"Adjusting the Recording Length")
